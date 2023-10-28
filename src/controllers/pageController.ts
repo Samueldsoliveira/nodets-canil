5a928cdfb69b62b3a3a2cef35b3c90eed4007e1f
Samuel Oliveira <samuelzinS10@gmail.com>
2023-10-27 23:08:27 -0300

    separating-the-views-and-adding-pages-and-partials

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,22 +1,24 @@
 import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-    res.send("Home no controller")
-    //res.render('pages/page');
+    res.render('pages/pages');
 
 }
 
 export const dogs = (req: Request, res: Response) => {
+    res.send('dogs')
     //res.render('pages/page');
 
 }
 
 export const cats = (req: Request, res: Response) => {
+    res.send('Cats')
     //res.render('pages/page');
 
 }
 
 export const fishes = (req: Request, res: Response) => {
+    res.send('Fishes')
     //res.render('pages/page');
 
 }
